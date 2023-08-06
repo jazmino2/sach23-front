df1825a5c584a79aab2585e4979a1811cc6383c3
gris34 <129626324+gris34@users.noreply.github.com>
2023-08-06 13:40:34 -0400

    commit inicial

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const nombreUsuario = document.getElementById("nombreUsuario");
+
+// Cambiar el nombre de usuario
+nombreUsuario.innerText = "Nuevo nombre de usuario";
+
+const ingresarFecha = document.getElementById("ingresarFecha");
+
+// Agregar evento al botón "Ingresar fecha"
+ingresarFecha.addEventListener("click", () => {
+  // Código para ingresar fecha
+});
+
+const verHorarios = document.getElementById("verHorarios");
+
+// Agregar evento al botón "Ver horarios"
+verHorarios.addEventListener("click", () => {
+  // Código para ver horarios
+});
